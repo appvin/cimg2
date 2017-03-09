cf71a61145504311da469febf637887762ca4ecb
appvin <appvin@outlook.com>
2017-03-09 21:34:18 +0800

    新增class

---
+++ cimg2/canny_class.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include "CImg.h"
+
+using namespace cimg_library;
+using namespace std;
+
+class canny_img
+{
+private:
+	CImg img;
+
+public:
+
+};
---
+++ cimg2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "canny.h"
+using namespace std;
+
+int main()
+{
+	string infile = "input.bmp";
+	canny(infile);
+	
+	return 0;
+}
